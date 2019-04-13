eda3a93918e2d07cf96510cf3f8d42a5908f1c22
Felix Zapata <fzberlinches@gmail.com>
2019-04-13 11:11:56 +0200

    docs(listbox-combobox.js): udpate description

--- listbox-combobox.js
+++ listbox-combobox.js
@@ -1,6 +1,6 @@
 /**
  * `listbox-combobox`
- * Polymer component based on the ARIA 1.1 Combobox with Listbox Popup Examples
+ * LitElement component based on the ARIA 1.1 Combobox with Listbox Popup Examples
  * https://www.w3.org/TR/wai-aria-practices/examples/combobox/aria1.1pattern/listbox-combo.html
  *
  */
